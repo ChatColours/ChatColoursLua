7d6d895fd3d793ad9aed058d8be7b832a95fcf21
ChatColours <coolnoob5008@gmail.com>
2026-04-26 13:46:18 +0800

    Update main.lua

--- Bot/main.lua
+++ Bot/main.lua
@@ -1,133 +1,97 @@
--- SET UP
 local owner = getgenv().Owner
-local bots = getgenv().Bots
-local prefix = getgenv().Prefix or ";"
-local render = getgenv().render or true
-local useAPI = getgenv().useAPI or false
-local api = getgenv().API
+local useapi = --[[getgenv().useAPI or]] false -- if true, all commands will be sent to the server
+local api = --[[getgenv().API]] "e" -- if u have an api plez put here or ask chatcolours for one
 
-if not owner or not bots or (useAPI and not api) then -- Make sure settings have been set up properly.
+-- I fucking hate femboys
+local HTTP = game:GetService("HttpService")
+local plr = game.Players.LocalPlayer
+
+if useapi == true and not api then
 	warn([[
 [CONFIG ERROR]
 Some settings are not set up correctly.
 
-Required:
-- owner must be a string
-- bots must be a table
-- if useAPI is true, api must be a string
+UseApi is set as 'true', but API is nil.
 ]])
 end
-
--- SERVICES
-local Services = {}
-
-Services.Players = game:GetService("Players")
-Services.RunService = game:GetService("RunService")
-Services.ReplicatedStorage = game:GetService("ReplicatedStorage")
-Services.TextChatService = game:GetService("TextChatService")
-Services.Debris = game:GetService("Debris")
-Services.HTTP = game:GetService("HttpService")
-Services.UserInputService = game:GetService("UserInputService")
-
--- MAIN
-local function CreateTag(User, Owner, Index)
-	local Billboard = Instance.new("BillboardGui")
-	local Canvas = Instance.new("CanvasGroup", Billboard)
-	local Own = Instance.new("TextLabel", Canvas)
-	local usr = Instance.new("TextLabel", Canvas)
-	local Ind = Instance.new("TextLabel", Canvas)
-	local Frame = Instance.new("Frame", Canvas)
-	local UICorner = Instance.new("UICorner", Canvas)
-	local UIStroke = Instance.new("UIStroke", Canvas)
-	local UICorner2Frame = Instance.new("UICorner", Frame)
-	-- PROPERTIES
-	Billboard.Size = UDim2.new(5, 0, 2, 0)
-	Billboard.StudsOffset = Vector3.new(0,3,0)
-	Canvas.Size = UDim2.new(0.95, 0, 0.95, 0)
-	Canvas.AbsolutePosition = Vector2.new(0.5, 0.5)
-	Canvas.Position = UDim2.new(0.5, 0, 0.5, 0)
-	UICorner.CornerRadius = UDim.new(0.05, 0)
-	UIStroke.StrokeSizingMode = Enum.StrokeSizingMode.ScaledSize
-	UIStroke.Thickness = 0.02
-	UICorner2Frame.CornerRadius = UDim.new(1, 0)
-	
-	Own.AnchorPoint = Vector2.new(1,0)
-	Own.Position = UDim2.new(1, 0, 0.013, 0)
-	Own.Size = UDim2.new(0.95, 0, 0.219, 0)
-	Own.BackgroundTransparency = 1
-	Own.TextScaled = true
-	Own.RichText = true
-	Own.Text = Owner
-	Ind.AnchorPoint = Vector2.new(1,0)
-	Ind.Position = UDim2.new(1, 0, 0.211, 0)
-	Ind.Size = UDim2.new(0.95, 0, 0.219, 0)
-	Ind.BackgroundTransparency = 1
-	Ind.TextScaled = true
-	Ind.RichText = true
-	Ind.Text = Index
-	usr.AnchorPoint = Vector2.new(1,0)
-	usr.Position = UDim2.new(1, 0, 0.429, 0)
-	usr.Size = UDim2.new(0.95, 0, 0.4, 0)
-	usr.BackgroundTransparency = 1
-	usr.TextScaled = true
-	usr.RichText = true
-	usr.Text = User
-	return Billboard
-end
-
-Services.RunService:Set3dRenderingEnabled(render)
-local plr = Services.Players.LocalPlayer
-local char = plr.Character or plr.CharacterAdded:Wait()
-
-if useAPI then
-	local WS
-	local succ, err = pcall(function()
+local WS
+if useapi == true then
+	local success, err = pcall(function()
 		WS = WebSocket.connect(api)
 	end)
-	if not succ and not WS then -- Fallback in case err idk
+	if not success then
 		game:GetService("StarterGui"):SetCore("SendNotification", {
 			Title = "ChatColours-Script",
 			Text = "ERROR: Please check console",
 			Duration = 5
 		})
-		warn(`ChatColours-Script:ERROR:WebSocket failed to connect to ({API}) Are you sure this is the right URL?`)
-	elseif succ and WS then
-		local data = {
-			UserId = plr.UserId,
-			Name = plr.Name,
-			Owner = owner,
-			BotsList = bots
-		}
-		WS:Send(Services.HTTP:JSONEncode(data)) -- Send message to API
-		WS.OnMessage:Connect(function(msg)
-			local data = Services.HTTP:JSONDecode(msg)
-			if data then
-				if data.type == "userconnected" then
-					local newBot = Services.Players:GetPlayerByUserId(data.Botuser)
-					if newBot and table.find(bots, newBot.Name) then
-						if newBot.Character then
-							local newBillboard = CreateTag(newBot.Name, owner, table.find(bots, newBot.Name))
-
-							newBillboard.Parent = char
-							local head = char:WaitForChild("Head")
-							if head then
-								newBillboard.Adornee = head
-							end
-						end
-						newBot.CharacterAdded:Connect(function(char)
-							local newBillboard = CreateTag(newBot.Name, owner, table.find(bots, newBot.Name))
-							
-							newBillboard.Parent = char
-							local head = char:WaitForChild("Head")
-							if head then
-								newBillboard.Adornee = head
-							end
-						end)
-					end
-				end
-			else
-				warn(`ChatColours-Script:ERROR:JSONDecode failed when receiving message from API ({api}), are you sure the API has sent a table?`)
-			end
-		end)
+		warn(`ChatColours-Script:ERROR:WebSocket failed to connect to ({api}) Are you sure this is the right URL?`)
 	end
 end
+if WS and useapi then
+	game:GetService("StarterGui"):SetCore("SendNotification", {
+		Title = "ChatColours-Script",
+		Text = "Connected to API!",
+		Duration = 5
+	})
+	
+	WS.OnMessage:Connect(function(msg)
+		local data = HTTP:JSONDecode(msg)
+
+		if data.type == "status_result" then
+			for _, user in ipairs(data.data) do
+				print(user.Name, user.Owner, user.UserId)
+			end
+		end
+	end)
+end
+local WindUI = loadstring(game:HttpGet("https://github.com/Footagesus/WindUI/releases/latest/download/main.lua"))()
+
+local Window = WindUI:CreateWindow({
+	Title = "Bot hub",
+	Icon = "door-open", -- lucide icon
+	Author = "By ChatColours, free & open-source",
+	Folder = "ChatsBot",
+
+	-- ↓ This all is Optional. You can remove it.
+	Size = UDim2.fromOffset(580, 460),
+	MinSize = Vector2.new(560, 350),
+	MaxSize = Vector2.new(850, 560),
+	ToggleKey = Enum.KeyCode.LeftShift,
+	Transparent = true,
+	Theme = "Dark",
+	Resizable = true,
+	SideBarWidth = 200,
+	BackgroundImageTransparency = 0.42,
+	HideSearchBar = true,
+	ScrollBarEnabled = false,
+
+	User = {
+		Enabled = true,
+		Anonymous = false,
+		Callback = function()
+			--print("clicked")
+		end,
+	},
+})
+
+local Tab = Window:Tab({
+	Title = "API",
+	Icon = "bird", -- optional
+	Locked = not useapi,
+})
+
+local Button = Tab:Button({
+	Title = "Get bot status",
+	Desc = "Test Button",
+	Locked = false,
+	Callback = function()
+		if WS then
+			local data = {
+				type = "status",
+				user = owner
+			}
+			WS:SendMessage(HTTP:JSONEncode(data))
+		end
+	end
+})
